9ad8c12e65f0de9bdc3b8fd913a973264ba5b3bd
shivam kumar <sk8612181@gmail.com>
2025-10-02 19:37:37 +0530

    feat: add solution for number of islands problem
    Implement depth-first search algorithm to count islands in a 2D grid. The solution marks visited cells to avoid double counting and checks all four directions (up, down, left, right) for connected land cells.

---
+++ Graph/NumberOfIslands.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    public:
+        void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& vis, int m , int n){
+          
+          if(i <0 || j<0 || i>=m  || j>=n || vis[i][j] || grid[i][j] =='0'  ){
+            return ;
+          }
+    
+          vis[i][j] = true;
+           dfs(i-1, j, grid, vis, m, n );
+           dfs(i+1, j, grid, vis, m, n );
+           dfs(i, j-1, grid, vis, m, n );
+           dfs(i, j+1, grid, vis, m, n );
+        }
+    
+        int numIslands(vector<vector<char>>& grid) {
+            int no_of_islands = 0;
+            int m = grid.size();
+            int n = grid[0].size();
+    
+            vector<vector<bool>> vis(m, vector<bool>(n, false));
+    
+            for(int i=0; i<m ; i++){
+                for(int j=0; j<n; j++){
+                    if(grid[i][j] == '1' && !vis[i][j]){
+                    dfs(i, j, grid, vis, m, n );
+                     no_of_islands ++;
+                    }
+                }
+            }
+         return no_of_islands;
+        }
+    };
+    
